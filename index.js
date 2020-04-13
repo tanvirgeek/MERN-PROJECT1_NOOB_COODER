48c3e2e59a46f2b7bd51e16dc774ca8e96303772
tanvirgeek <tanvirgeek@gmail.comm>
2020-04-13 09:22:55 +0600

    Connected to Database

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const body_paeser = require('body-parser');
+
+const app = express();
+app.use(body_paeser.json());
+
+//routes
+//const employee = require('./routes/employee');
+//app.use('/employee', employee);
+
+mongoose.connect('mongodb://localhost:27017/noob_cooder_mern',{
+    useNewUrlParser: true,
+    useFindAndModify: false
+}, (err)=>{
+        if(err){
+            process.exit(1);
+            console.log('unable to connet to database');
+        }else{
+            console.log("successfully conneted to database");
+        }
+    }
+)
+
+const port = process.env.PORT || 3000;
+app.listen(port, ()=>{
+    console.log(`app is running on port: ${port}`);
+});
